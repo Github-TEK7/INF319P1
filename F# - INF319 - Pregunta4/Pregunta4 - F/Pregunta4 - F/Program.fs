0a2b54f595739208ba95e1e38d2cd1c5cc8c571e
Github-TEK7 <85852130+Github-TEK7@users.noreply.github.com>
2021-09-22 00:10:29 -0400

    Add files via upload

---
+++ F# - INF319 - Pregunta4/Pregunta4 - F/Pregunta4 - F/Program.fs
@@ -0,0 +1,18 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+[<EntryPoint>]
+let main argv =
+    
+    //let lista =[1..10]
+    let lista = [2;34;3;7;16;1;13;4;21;8]
+    let listap = [ for i in lista do if i%2=0 then yield i ]
+    let listai = [ for i in lista do if i%2<>0 then yield i ]
+
+    printfn "La lista original es: %A" lista
+    printfn "La lista de pares es: %A" listai
+    printfn "La lista de impares es: %A" listap
+
+    
+    0 // return an integer exit code
