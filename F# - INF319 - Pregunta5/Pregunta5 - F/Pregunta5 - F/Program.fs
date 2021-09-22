321e501d159938d6a09e105c2a9a0137f9e13039
Github-TEK7 <85852130+Github-TEK7@users.noreply.github.com>
2021-09-22 00:11:50 -0400

    Add files via upload

---
+++ F# - INF319 - Pregunta5/Pregunta5 - F/Pregunta5 - F/Program.fs
@@ -0,0 +1,18 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+let function5 x y =
+  let mutable s=0
+  for i in (1) .. (y) do
+    s<-s+x
+  s
+
+
+[<EntryPoint>]
+let main argv =
+    
+    let a=function5 5 7
+    printfn "Multipliacion: 5*7=%i" a
+
+    0 // return an integer exit code
